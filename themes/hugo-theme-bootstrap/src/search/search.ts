67f469798f2b4d0728ab88977d61e068ada40472
axaneamului <contact@axaneamului.ro>
2022-01-13 01:19:51 +0200

    fix search by author

--- themes/hugo-theme-bootstrap/src/search/search.ts
+++ themes/hugo-theme-bootstrap/src/search/search.ts
@@ -233,6 +233,7 @@ export class Search {
         tags: result.item.tags,
         series: result.item.series,
         score: Search.formatScore(result.score),
+        author: result.item.author,
         date: result.item.date,
         url() {
           return Search.normalizeTaxonomy;
